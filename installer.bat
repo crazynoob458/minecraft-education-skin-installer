e3fd3c28136bc48ac82ca988d71f8e0b5bed2732
tim suh <65052972+crazynoob458@users.noreply.github.com>
2020-06-12 11:58:49 +1000

    hotfix

--- installer.bat
+++ installer.bat
@@ -1,16 +1,16 @@
 @echo off
-echo minecraft skin installer ver 0.1
+echo minecraft skin installer ver 0.2
 echo please run this batch file in ADMIN
 echo also if you installed minecraft education edition on uwp mode *microsoft store* well die in a hole why would you?
 ready?
 pause
 cls
-echo please put this bat in a clean folder WITHOUT any file in it with the skin file please name the skin skin.png
-echo folder structure must look like this skin.png skins.json installer.bat
+echo please place skins.json and your skin renamed to skin.png it is also recommended to place installer.bat on C:/
 echo press enter when youre ready
 pause
 cls
 echo log
+cd C:\
 echo folder structure is
 @echo on
 dir
