2153a64407fea164bcd72d69b937f0c4b7c6609f
tim suh <65052972+crazynoob458@users.noreply.github.com>
2020-06-12 10:57:13 +1000

    created base currently unusable

--- installer.bat
+++ installer.bat
@@ -1,1 +1,14 @@
-echo work in progress...
+@echo off
+echo minecraft skin installer
+echo please run this batch file in ADMIN
+echo also if you installed minecraft education edition on uwp mode *microsoft store* well die in a hole why would you?
+ready?
+pause
+cls
+echo please put this bat in a clean folder WITHOUT any file in it with the skin file please name the skin skin.png
+echo press enter when youre ready
+pause
+cls
+@echo on
+dir
+@echo off
