1bd25e16a7af37488627453ba6f06f111cdab08d
tim suh <65052972+crazynoob458@users.noreply.github.com>
2020-06-15 12:01:36 +1000

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -2,7 +2,7 @@
 echo minecraft skin installer ver 0.3
 echo please run this batch file in ADMIN
 echo also if you installed minecraft education edition on uwp mode *microsoft store* well die in a hole why would you?
-ready?
+echo ready?
 pause
 cls
 echo please place skins.json and your skin renamed to skin.png it is also recommended to place installer.bat on C:/
@@ -15,8 +15,8 @@ echo folder structure is
 @echo on
 dir
 echo copying skin.png
-copy "skin.png" "C:\Program Files (x86)\Microsoft Studios\Minecraft Education Edition\data\skin_packs\edu\Developers
-echo copying skins.json"
+copy "skin.png" "C:\Program Files (x86)\Microsoft Studios\Minecraft Education Edition\data\skin_packs\edu\Developers"
+echo copying skins.json
 copy /Y "skins.json" "C:\Program Files (x86)\Microsoft Studios\Minecraft Education Edition\data\skin_packs\edu\Developers"
 @echo off
 echo complete! open minecraft education edition and your skin should be in the developers section
