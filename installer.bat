60501cdde8cc55fc045c4653ad50490f93400c1e
tim suh <65052972+crazynoob458@users.noreply.github.com>
2020-06-25 13:08:37 +1000

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -5,6 +5,7 @@ echo ready?
 set a=NUL
 set d=NUL
 set x=NUL
+set q=NUL
 echo press a key to continue
 pause>NUL
 cls
@@ -20,14 +21,16 @@ echo drap and drop your skin into this command prompt
 echo remember to rename skin to skin.png
 set /p a=skin please!:
 cls
+echo i need skins.json can you download it (or its in the folder)
+set /p q=skins.json please!
 echo log
 echo folder structure is
 @echo on
 dir
 echo copying skin.png
-copy "%a%" "%x%\data\skin_packs\edu\developers"
+copy /Y "%a%" "%x%\data\skin_packs\edu\developers"
 echo copying skins.json
-copy /Y "skins.json" "%x%\data\skin_packs\edu\developers"
+copy /Y "%q%" "%x%\data\skin_packs\edu\developers"
 @echo off
 echo complete! open minecraft education edition and your skin should be in the developers section
 pause
@@ -39,5 +42,10 @@ set /p x=where is minecraft installed?
 goto start
 :defaultdir
 cls
+echo before we go we need admin permission
+echo close this window and right-click the batch file, and click run as administrator
+echo the click yes
+echo if it prompts you to enter a admin password sorry i cant do anything for you please contact
+echo device administrator
 set %x%=C:\Program Files (x86)\Microsoft Studios\Minecraft Education Edition
 goto start
